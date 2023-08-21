2276ee3668195f510459f644eaf401e849309da0
Marwan <marwan3lewa@yahoo.com>
2023-08-21 02:36:14 -0700

    kooooo

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,13 +2,12 @@
 /**
  * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
- * Return: void 
  */
 void _puts(char *str)
 {
-	for (; *str != '\0': str++ )
+	while (*str != '\0')
 	{
-		_putchar(*str);
+		_putchar(*str++);
 	}
 		_putchar('\n');
 }
